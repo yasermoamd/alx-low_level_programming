88b06f0cb41d09e44d442be83afb5edef2559ea1
yasermoamd <yasermoamd@hotmail.com>
2023-09-08 12:44:17 -0700

    print all alphabet accept e q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - start of program
+ *
+ * Return: nothing if successful
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	putchar('\n');
+	return (0);
+}
