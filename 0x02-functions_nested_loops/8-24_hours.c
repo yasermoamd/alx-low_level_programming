d7d4864d4baa3810af5e1c730c813d1a69a27db9
yasermoamd <yasermoamd@hotmail.com>
2023-09-12 14:42:48 -0700

    print hour and minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - print every hour and minutes
+ *
+ * Return: nothing if successful
+ */
+int jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 23; i++)
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar('0' + i / 10);
+			_putchar(i % 10 + '0');
+			_putchar (':');
+			_putchar ('0' + j / 10);
+			_putchar ('0' + j % 10);
+			putchar ('\n');
+		}
+}
