587e494a4a22a236e98041f8586a1c88f12630bc
yasermoamd <yasermoamd@hotmail.com>
2023-09-12 10:15:42 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - single char
+ *
+ * Return: single chars
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
