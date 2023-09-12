addb42230263ef8dc91ed2be54760f4db79b4f86
yasermoamd <yasermoamd@hotmail.com>
2023-09-12 13:16:00 -0700

    print absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _abs - compute the abslute value
+ * @x: integer
+ *
+ * Return: The absolute value of n
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (-x);
+	}
+}
