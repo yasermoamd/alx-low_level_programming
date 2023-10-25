3cbb66b3d401bc9b81e85a1a42811ba66f0b685a
yasermoamd <yasermoamd@hotmail.com>
2023-10-24 21:36:37 -0700

    length of element in linkedlist

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - length of element in linkedlist.
+ * @h: head of linkedlist.
+ * Return: the count of elements.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		counter++;
+	}
+
+	return (counter);
+}
