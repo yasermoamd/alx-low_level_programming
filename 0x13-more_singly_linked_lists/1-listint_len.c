619d060fabe64b6c4a02ba50b24828685ed1a884
yasermoamd <yasermoamd@hotmail.com>
2023-10-24 21:42:35 -0700

    length of element

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,13 +9,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t counter = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-	h = h->next;
-		counter++;
+		h = h->next;
+		count++;
 	}
 
-	return (counter);
+	return (count);
 }
