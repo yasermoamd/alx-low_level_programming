d7f32a00805b518bb2eaa00a1d8c80c5eaf8cdc9
yasermoamd <yasermoamd@hotmail.com>
2023-10-25 11:48:34 -0700

    delete node in certain index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - eletes the node at index index of a listint_t linked list.
+ * delete_nodeint_at_index - eletes the node at index index of a list.
  * @head: head of list of int.
  * @index: index of element in linkedlist.
  * Return: 1 if it succeeded, -1 if it failed.
