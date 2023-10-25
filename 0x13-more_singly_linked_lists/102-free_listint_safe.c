73999f1b7214e43a940c5510eb3e1f810b21e4a1
yasermoamd <yasermoamd@hotmail.com>
2023-10-25 15:18:06 -0700

    free listint safe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,19 +9,55 @@
  * Return: pointer to the new list
  */
 
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+{
+	listint_t **newls;
+	size_t j;
+
+	newls = malloc(size * sizeof(listint_t *));
+	if (newls == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (j = 0; j < size - 1; j++)
+		newls[j] = list[j];
+	newls[j] = new;
+	free(list);
+	return (newls);
+}
+
+/**
+ * free_listint_safe - frees a listint_t linked list.
+ * @head: double pointer to the start of the list
+ * Return: the number of nodes in the list
+ */
+
 size_t free_listint_safe(listint_t **head)
 {
-	size_t count = 0;
-	listint_t *current = *head;
+	size_t j, num = 0;
+	listint_t **list = NULL;
+	listint_t *next;
 
-	while (current != NULL)
+	if (head == NULL || *head == NULL)
+		return (num);
+	while (*head != NULL)
 	{
-		listint_t *next = current->next;
-		free(current);
-		current = next;
-		count++;
+		for (j = 0; j < num; j++)
+		{
+			if (*head == list[j])
+			{
+				*head = NULL;
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = _ra(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
-
-	*head = NULL;
-	return count;
+	free(list);
+	return (num);
 }
