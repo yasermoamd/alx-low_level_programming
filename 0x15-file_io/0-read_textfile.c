caa5c72869b4604f28e119dc1e7aa1904e3cdcf8
yasermoamd <yasermoamd@hotmail.com>
2023-10-30 13:24:01 -0700

    function that reads a text file and prints

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and print.
+ * @filename: constant char.
+ * @letters: varidic size_t.
+ * Return: 0 if can not read or open.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t open_file, read_file, write_file;
+
+	char *p;
+
+	if (filename == NULL)
+		return (0);
+
+	p = malloc(sizeof(char) * letters);
+	if (p == NULL)
+		return (0);
+
+	open_file = open(filename, O_RDONLY);
+	read_file = read(open_file, p, letters);
+	write_file = write(1, p, read_file);
+
+	if (open_file == -1 || read_file == -1)
+	{
+		return (0);
+		free(p);
+	}
+	free(p);
+	close(open_file);
+	return (write_file);
+}
