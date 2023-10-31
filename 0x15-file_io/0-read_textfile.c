1ecdafe6bb0d6eb47447c202adead6c3bcff988c
yasermoamd <yasermoamd@hotmail.com>
2023-10-31 01:23:09 -0700

    fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t read_file, write_file;
+	ssize_t open_file, read_file, write_file;
 	char *p;
 
 	if (filename == NULL || letters == 0)
@@ -18,25 +18,35 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	p = malloc(sizeof(char) * letters);
-
 	if (p == NULL)
-		return (0);
-
-	read_file = read(open(filename, O_RDONLY), p, letters);
-	if (read_file == -1)
 	{
-		free(p);
-		return (0);
+		return 0;
 	}
 
-	write_file = write(STDOUT_FILENO, p, read_file);
+	open_file = open(filename, O_RDONLY);
+	if (open_file == -1)
+	{
+		free(p);
+		return 0;
+	}
 
+	read_file = read(open_file, p, letters);
+	if (read_file == -1)
+	{
+		close(open_file);
+		free(p);
+		return 0;
+	}
+
+	close(open_file);
+
+	write_file = write(STDOUT_FILENO, p, read_file);
 	if (write_file != read_file)
 	{
 		free(p);
-		return (0);
+		return 0;
 	}
 
 	free(p);
-	return (read_file);
+	return read_file;
 }
