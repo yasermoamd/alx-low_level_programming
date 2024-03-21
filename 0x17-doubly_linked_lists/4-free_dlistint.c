7afe894c73776b2eeb2b1e54b752b10956eff0ca
yasermoamd <yasermoamd@hotmail.com>
2024-03-21 21:30:08 +0000

    free dlist

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* free_dlistint -function that frees a dlistint_t list.
+* @head: the head
+*/
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *current = head;
+
+    while (current != NULL)
+    {
+        free(current);
+        head = head->next;
+        current = head;
+    }
+
+}
