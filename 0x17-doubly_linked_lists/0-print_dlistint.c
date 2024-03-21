dd72214dbfcdd0b4ea5642d188ed09c7addcb41f
yasermoamd <yasermoamd@hotmail.com>
2024-03-21 13:47:23 +0000

    fix: print dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,4 +8,14 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
+    size_t count = 0;
+
+    while (h != NULL)
+    {
+        printf("%d\n", h->data);
+        h = h->next;
+        count++;
+    }
+    
+    return (count);
 }
