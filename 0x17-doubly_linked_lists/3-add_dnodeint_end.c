38aec718c7baf059910a8b60d01794ba79e764c8
yasermoamd <yasermoamd@hotmail.com>
2024-03-21 21:26:50 +0000

    add el in end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,21 +11,25 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 	dlistint_t *current;
 
-	current = *head;
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->prev = NULL;
 	new_node->n = n;
 	new_node->next = NULL;
 
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	current = *head;
 	while (current->next != NULL)
 		current = current->next;
-	current->next = new_node;
 	new_node->prev = current;
+	current->next = new_node;
 	return (*head);
 
 }
